e08ad5eb358fe65b4935c064a097d7254b1a1dfe
dnd_constuctive <dnd@constructive.io>
2022-03-14 16:07:56 +0100

    set clickable task

--- i-robot.js
+++ i-robot.js
@@ -2,82 +2,131 @@ class RobotContainer {
     #columns = []
 
     addColumn = (column) => {
-        if(!(column instanceof RobotColumn)) throw new Error('Not valid column.')
-        if(this.#columns.length > 3)  throw new Error('Maximum number of column is 4.')
+        if (!(column instanceof RobotColumn)) throw new Error('Not valid column.')
+        if (this.#columns.length > 3) throw new Error('Maximum number of column is 4.')
         this.#columns.push(column)
     }
 
-    renderHTML = () =>{
+    renderHTML = () => {
         let div = document.createElement('div');
         div.classList.add('column-holder');
-        this.#columns.forEach((c)=>{div.append(c.renderHTML())});
+        this.#columns.forEach((c) => { div.append(c.renderHTML()) });
         return div;
     }
 
 }
 
-class RobotColumn{
+class RobotColumn {
     #tasks = [];
     #name = '';
-    constructor(name){
+    step = 1;
+    constructor(name, step) {
         this.#name = name;
+        this.step = step;
     }
 
     addTask = (task) => {
-        if(!(task instanceof RobotTask)) throw new Error('This is not task.');
+        if (!(task instanceof RobotTask)) throw new Error('This is not task.');
         this.#tasks.push(task);
+    }
+
+    setNotCompleted = (task) => {
+        let newTasks = this.#tasks.filter((n => { return n.step !== task.step }))
+        this.#tasks = newTasks;
     }
 
     renderHTML = () => {
         let div = document.createElement('div');
         div.classList.add('column');
         div.innerHTML = this.#name;
-        this.#tasks.forEach(t => {div.append(t.renderHTML())});
+        this.#tasks.forEach(t => { div.append(t.renderHTML()) });
         return div;
     }
 }
 
-class RobotTask{
-    #name = '';
+class RobotTask {
+    name = '';
     #created = new Date();
-    constructor(name){
-        this.#name = name;
+    #status = '';
+    step = 1;
+    #callback = () => { };
+    constructor(name, status) {
+        this.name = name;
+        this.#status = status;
     }
+    onClick = (callback) => {
+        this.#callback = callback;
+    }
+    setNextStep = () => {
+        if (this.#status === 'Backlog') {
+            this.#status = 'ToDo';
+            this.step = 2;
+            return;
+        }
+        if (this.#status === 'ToDo') {
+            this.#status = 'In Progress';
+            this.step = 3;
+            return;
+        }
+        if (this.#status === 'In Progress') {
+            this.#status = 'Done';
+            this.step = 4;
+            return;
+        }
+    }
+
+    getName = () => { return this.name }
+
     renderHTML = () => {
         let div = document.createElement('div');
         div.classList.add('task');
-        div.innerHTML = this.#name + this.#created.toLocaleDateString();
+        div.addEventListener('click', () => this.#callback())
+        div.innerHTML = this.name + " " + this.#created.toLocaleDateString();
         return div;
     }
 }
 
 let container = new RobotContainer();
-let column1 = new RobotColumn('Backlog');
-let column2 = new RobotColumn('ToDo');
-let column3 = new RobotColumn('In Progress');
-let column4 = new RobotColumn('Done');
+let column1 = new RobotColumn('Backlog', 1);
+let column2 = new RobotColumn('ToDo', 2);
+let column3 = new RobotColumn('In Progress', 3);
+let column4 = new RobotColumn('Done', 4);
 
-let task1 = new RobotTask('1 Task');
-let task2 = new RobotTask('2 Task');
-let task3 = new RobotTask('2 Task');
-
-// class iRobot{
-//     #
-//     constructor(){}
-    
-
-// }
-
+let task1 = new RobotTask('Task 1 ', 'Backlog');
 
 column1.addTask(task1);
-column1.addTask(task2);
-column1.addTask(task3);
 
 container.addColumn(column1);
 container.addColumn(column2);
 container.addColumn(column3);
 container.addColumn(column4);
-console.log('cont', container)
 
-let html = document.querySelector('#root');
-html.appendChild(container.renderHTML());
+task1.onClick(() => {
+    task1.setNextStep();
+    column1.setNotCompleted(task1);
+    column2.setNotCompleted(task1);
+    column3.setNotCompleted(task1);
+    column4.setNotCompleted(task1);
+    if (task1.step === 1)
+        column1.addTask(task1)
+    if (task1.step === 2)
+        column2.addTask(task1)
+
+    if (task1.step === 3)
+        column3.addTask(task1)
+
+    if (task1.step === 4)
+        column4.addTask(task1)
+
+    render();
+})
+
+
+
+function render() {
+    let html = document.querySelector('#root');
+    html.innerHTML = "";
+    html.appendChild(container.renderHTML());
+} render();
+
+
